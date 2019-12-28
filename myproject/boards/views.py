9f964b7c437ff5c0550c35bd3a64e03c0f0781bf
Jose Chacon <josejch11@gmail.com>
2019-12-27 19:37:38 -0500

    first commit

---
+++ myproject/boards/views.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, Http404
+
+from boards.forms import NewTopicForm
+from boards.models import Board, Post
+
+
+# Create your views here.
+
+def home(request):
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    return render(request, 'home.html', {'boards' : boards})
+
+def board_topics(request ,pk):
+    '''
+    try:
+        board = Board.objects.get(pk = pk)
+    except Board.DoesNotExist:
+        raise Http404
+    '''
+    board = get_object_or_404(Board,pk=pk)
+    return render(request,'topics.html', {'board' : board})
+
+def new_topic(request,pk):
+
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # TODO: get the currently logged in user
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
